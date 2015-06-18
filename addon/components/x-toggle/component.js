knownasilya/ember-toggle

commit 8ed34e96349b2b1f3e9e9d60b93ea10729bca611

Move component definition to addon folder

addon/components/x-toggle/component.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import ENV from '../config/environment';
+import layout from './template';
 
 const {
   on,
@@ -10,6 +11,7 @@ const {
 const config = ENV['ember-cli-toggle'] || {};
 
 export default Ember.Component.extend({
+  layout: layout,
   tagName: 'span',
   classNameBindings: ['toggled:x-toggle-container-checked', 'disabled:x-toggle-container-disabled'],
   classNames: ['x-toggle-container'],

app/components/x-toggle/component.js
@@ -1,0 +1,1 @@
+export { default } from 'ember-cli-toggle/components/x-toggle/component';

tests/unit/components/x-toggle/component-test.js
@@ -1,0 +1,19 @@
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('x-toggle', 'Unit | Component | x toggle', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar'],
+  unit: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Creates the component instance
+  var component = this.subject();
+  assert.equal(component._state, 'preRender');
+
+  // Renders the component to the page
+  this.render();
+  assert.equal(component._state, 'inDOM');
+});

paulwittmann/ember-cli-toggle

commit b48f03761e202f3171f84a423cb4ca12964afd86

Move template back to app/templates/components/x-toggle.hbs
Then change in 8ed34e96349b broke the template loading

addon/components/x-toggle/component.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import layout from './template';
 
 const {
   on,
@@ -9,7 +8,6 @@ const {
 } = Ember;
 
 export default Ember.Component.extend({
-  layout: layout,
   tagName: 'span',
   disabled: false,
   isToggled: computed.oneWay('toggled'),

app/templates/components/x-toggle.hbs
@@ -1,0 +1,19 @@
+{{#if showLabels}}
+	<span class="toggle-text toggle-prefix {{size}}">
+		{{offLabel}}
+	</span>
+{{/if}}
+
+{{input id=forId type='checkbox' checked=isToggled class='x-toggle' disabled=disabled}}
+
+<label class="x-toggle-btn {{themeClass}} {{size}} {{if disabled 'x-toggle-disabled'}}"
+  for={{forId}}
+  data-tg-off={{offLabel}}
+  data-tg-on={{onLabel}}>
+</label>
+
+{{#if showLabels}}
+	<span class="toggle-text toggle-postfix {{size}}">
+		{{onLabel}}
+	</span>
+{{/if}}